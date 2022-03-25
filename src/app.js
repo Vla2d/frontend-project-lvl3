baa62d9789867b181cb30d58b86eba5335ea883c
Vlad <fireflay.2005@gmail.com>
2022-03-25 12:19:40 +0100

    i18next added

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 import onChange from 'on-change';
 import * as yup from 'yup';
+import i18next from 'i18next';
 import { parserRSS, TypeError } from './utils.js';
 import render from './view.js';
+import ru from './locales/ru.js';
 
 const schema = yup.string().url().required();
 
+i18next.init({
+  lng: 'ru',
+  debug: true,
+  resources: {
+    ru,
+  },
+});
+
 const errors = {
-  required: 'Empty URL input',
-  url: 'Invalid URL',
-  rss: 'Invalid RSS data',
-  sameUrl: 'URL already exists in the list',
-  network: 'Network error',
+  required: i18next.t('errors.required'),
+  url: i18next.t('errors.url'),
+  rss: i18next.t('errors.rss'),
+  sameUrl: i18next.t('errors.sameUrl'),
+  network: i18next.t('errors.network'),
 };
 
 export default () => {
